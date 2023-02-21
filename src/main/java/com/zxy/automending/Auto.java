a62e851bae4604b4072d38d3bee7a296ebe5a34d
zxy <1141927681@qq.com>
2023-02-22 07:17:34 +0800

    1

--- src/main/java/com/zxy/automending/Auto.java
+++ src/main/java/com/zxy/automending/Auto.java
@@ -15,54 +15,49 @@ import java.util.ArrayList;
 
 
 public class Auto {
-    static Auto auto ;
+    static Auto auto;
     MinecraftClient mc;
     ClientPlayerEntity player;
     int temp = -1;
     boolean runing = false;
-    World world = null;
     int tick = 0;
     int toukui = -1;
     int xiongjia = -1;
     int kuzi = -1;
     int xiezi = -1;
 
-    public void autoMenDing(PlayerEntity player){
-        ScreenHandler sc =  player.currentScreenHandler;
-        if(runing && !sc.slots.get(45).getStack().isDamaged()) reSwitch();
+    public void autoMenDing(PlayerEntity player) {
+        ScreenHandler sc = player.currentScreenHandler;
+        if (runing && !sc.slots.get(45).getStack().isDamaged()) reSwitch();
         tick = 0;
-        if(mc==null || !player.equals(mc.player)) return;
-        if(player.getWorld().equals(world)) {
-            temp = -1;
-            tick = 0;
-            runing = false;
-        }
+        if (mc == null || !player.equals(mc.player)) return;
 
-        for(int i=0 ; i < sc.slots.size() ; i++){
+        for (int i = 0; i < sc.slots.size(); i++) {
             ItemStack item = sc.slots.get(i).getStack();
-            if(runing){
-                xiejia(i,sc);
+            if (runing) {
+                xiejia(i, sc);
                 continue;
             }
-            if(
-                    item.isOf(Items.AIR)||
-                    EnchantmentHelper.getLevel(Enchantments.MENDING,item)<=0 ||
-                    !item.isDamaged()||
-                    item.equals(player.getMainHandStack())||
-                    i==45 || i==5|| i==6|| i==7|| i==8
-            )continue;
-            autoSwitch(i,45);
+            if (
+                    item.isOf(Items.AIR) ||
+                            EnchantmentHelper.getLevel(Enchantments.MENDING, item) <= 0 ||
+                            !item.isDamaged() ||
+                            item.equals(player.getMainHandStack()) ||
+                            i == 45 || i == 5 || i == 6 || i == 7 || i == 8
+            ) continue;
+            autoSwitch(i, 45);
             temp = i;
             runing = true;
             break;
         }
     }
-    public void tick(){
-        if(!runing)return;
-        if(0 == ++tick % 10 ) reSwitch();
+
+    public void tick() {
+        if (!runing) return;
+        if (0 == ++tick % 10) reSwitch();
     }
 
-    public void autoSwitch(int a, int b) {
+    public synchronized void autoSwitch(int a, int b) {
         if (mc.interactionManager == null) {
             return;
         }
@@ -70,91 +65,70 @@ public class Auto {
             player.closeHandledScreen();
         }
         ScreenHandler screenHandler = player.currentScreenHandler;
-            mc.interactionManager.clickSlot(screenHandler.syncId, a, 0, SlotActionType.PICKUP, player);
-            mc.interactionManager.clickSlot(screenHandler.syncId, b, 0, SlotActionType.PICKUP, player);
-            mc.interactionManager.clickSlot(screenHandler.syncId, a, 0, SlotActionType.PICKUP, player);
+        mc.interactionManager.clickSlot(screenHandler.syncId, a, 0, SlotActionType.PICKUP, player);
+        mc.interactionManager.clickSlot(screenHandler.syncId, b, 0, SlotActionType.PICKUP, player);
+        mc.interactionManager.clickSlot(screenHandler.syncId, a, 0, SlotActionType.PICKUP, player);
 
     }
 
-    public void reSwitch(){
-        if(runing) {
-           if(temp!=-1) autoSwitch(temp, 45);
-           if(toukui!=-1) {
-               autoSwitch(toukui, 5);
-               toukui = -1;
-           }
-           if(xiongjia!=-1) {
-               autoSwitch(xiongjia, 6);
-               xiongjia = -1;
-           }
-           if(kuzi!=-1){
-               autoSwitch(kuzi, 7);
-               kuzi = -1;
-           }
-           if(xiezi!=-1) {
-               autoSwitch(xiezi, 8);
-               xiezi = -1;
-           }
+    public void reSwitch() {
+        if (runing) {
+            if (temp != -1) autoSwitch(temp, 45);
+            if (toukui != -1) {
+                autoSwitch(toukui, 5);
+                toukui = -1;
+            }
+            if (xiongjia != -1) {
+                autoSwitch(xiongjia, 6);
+                xiongjia = -1;
+            }
+            if (kuzi != -1) {
+                autoSwitch(kuzi, 7);
+                kuzi = -1;
+            }
+            if (xiezi != -1) {
+                autoSwitch(xiezi, 8);
+                xiezi = -1;
+            }
             tick = 0;
             temp = -1;
             runing = false;
         }
     }
-    public void xiejia(int i ,ScreenHandler sc){
-        ItemStack item = sc.slots.get(i).getStack();
-        if(item.isEmpty() || item.isDamaged() || EnchantmentHelper.getLevel(Enchantments.MENDING,item)<=0) return;
-        if(i==5 ){
-            for(; i < sc.slots.size();i++){
-                if(sc.slots.get(i).getStack().isOf(Items.AIR)){
-                    if(i==5||i==6||i==7||i==8||i==45) continue;
-                    toukui = i;
-                    autoSwitch(5,i);
-                    return;
+
+    public void xiejia(int q, ScreenHandler sc) {
+        ItemStack item = sc.slots.get(q).getStack();
+        if (!(q == 5 || q == 6 || q == 7 || q == 8) || item.isEmpty() || item.isDamaged() || EnchantmentHelper.getLevel(Enchantments.MENDING, item) <= 0)
+            return;
+        int air = -1;
+        for (int i = q; i < sc.slots.size(); i++) {
+            if (sc.slots.get(i).getStack().isOf(Items.AIR)) {
+                if (!(i == 5 || i == 6 || i == 7 || i == 8 || i == 45 ||
+                        i == temp || i == toukui || i == xiongjia || i == kuzi || i == xiezi )&&
+                                 (toukui == -1 || xiongjia == -1 || kuzi == -1 || xiezi == -1)) {
+                    air = i;
+                    break;
                 }
             }
         }
-        if(i==6 ){
-            for(; i < sc.slots.size();i++){
-                if(sc.slots.get(i).getStack().isOf(Items.AIR)){
-                    if(i==5||i==6||i==7||i==8||i==45) continue;
-                    xiongjia = i;
-                    autoSwitch(6,i);
-                    return;
-                }
-            }
-        }
-        if(i==7 ){
-            for(; i < sc.slots.size();i++){
-                if(sc.slots.get(i).getStack().isOf(Items.AIR)){
-                    if(i==5||i==6||i==7||i==8||i==45) continue;
-                    kuzi = i;
-                    autoSwitch(7,i);
-                    return;
-                }
-            }
-        }
-        if(i==8 ){
-            for( ; i < sc.slots.size();i++){
-                if(sc.slots.get(i).getStack().isOf(Items.AIR)){
-                    if(i==5||i==6||i==7||i==8||i==45) continue;
-                    xiezi = i;
-                    autoSwitch(8,i);
-                    return;
-                }
-            }
+        if (air == -1) return;
+        autoSwitch(q, air);
+        switch (q) {
+            case 5 -> toukui = air;
+            case 6 -> xiongjia = air;
+            case 7 -> kuzi = air;
+            case 8 -> xiezi = air;
         }
     }
 
 
-
-
-
-    public Auto(MinecraftClient client){
+    public Auto(MinecraftClient client) {
         this.mc = client;
         this.player = client.player;
         auto = this;
     }
-    public static Auto getAuto(){
+
+    public static Auto getAuto() {
         return auto;
     }
 }
