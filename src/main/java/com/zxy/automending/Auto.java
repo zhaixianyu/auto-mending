4cefdf999178002de1ca9adb0e902a46d449c889
zxy <1141927681@qq.com>
2023-02-22 09:28:37 +0800

    1

--- src/main/java/com/zxy/automending/Auto.java
+++ src/main/java/com/zxy/automending/Auto.java
@@ -20,11 +20,8 @@ public class Auto {
     ClientPlayerEntity player;
     int temp = -1;
     boolean runing = false;
+    ArrayList<Integer> zhuangbei;
     int tick = 0;
-    int toukui = -1;
-    int xiongjia = -1;
-    int kuzi = -1;
-    int xiezi = -1;
 
     public void autoMenDing(PlayerEntity player) {
         ScreenHandler sc = player.currentScreenHandler;
@@ -35,8 +32,8 @@ public class Auto {
         for (int i = 0; i < sc.slots.size(); i++) {
             ItemStack item = sc.slots.get(i).getStack();
             if (runing) {
-                xiejia(i, sc);
-                continue;
+                xiejia(sc);
+                break;
             }
             if (
                     item.isOf(Items.AIR) ||
@@ -74,21 +71,12 @@ public class Auto {
     public void reSwitch() {
         if (runing) {
             if (temp != -1) autoSwitch(temp, 45);
-            if (toukui != -1) {
-                autoSwitch(toukui, 5);
-                toukui = -1;
-            }
-            if (xiongjia != -1) {
-                autoSwitch(xiongjia, 6);
-                xiongjia = -1;
-            }
-            if (kuzi != -1) {
-                autoSwitch(kuzi, 7);
-                kuzi = -1;
-            }
-            if (xiezi != -1) {
-                autoSwitch(xiezi, 8);
-                xiezi = -1;
+            for (int i = 0; i < 4; i++) {
+                System.out.println(zhuangbei.get(i));
+                if (zhuangbei.get(i) != -1) {
+                    autoSwitch(zhuangbei.get(i), i + 5);
+                    zhuangbei.set(i, -1);
+                }
             }
             tick = 0;
             temp = -1;
@@ -96,28 +84,32 @@ public class Auto {
         }
     }
 
-    public void xiejia(int q, ScreenHandler sc) {
-        ItemStack item = sc.slots.get(q).getStack();
-        if (!(q == 5 || q == 6 || q == 7 || q == 8) || item.isEmpty() || item.isDamaged() || EnchantmentHelper.getLevel(Enchantments.MENDING, item) <= 0)
-            return;
-        int air = -1;
-        for (int i = q; i < sc.slots.size(); i++) {
-            if (sc.slots.get(i).getStack().isOf(Items.AIR)) {
-                if (!(i == 5 || i == 6 || i == 7 || i == 8 || i == 45 ||
-                        i == temp || i == toukui || i == xiongjia || i == kuzi || i == xiezi )&&
-                                 (toukui == -1 || xiongjia == -1 || kuzi == -1 || xiezi == -1)) {
-                    air = i;
-                    break;
+    public void xiejia(ScreenHandler sc) {
+        for (int q = 5; q < 9; q++) {
+            ItemStack item = sc.slots.get(q).getStack();
+            if (item.isEmpty() || item.isDamaged() || EnchantmentHelper.getLevel(Enchantments.MENDING, item) <= 0)
+                continue;
+            a:
+            for (int air = q; air < sc.slots.size(); air++) {
+                if (air == 5 || air == 6 || air == 7 || air == 8 || air == 45) continue;
+                if (sc.slots.get(air).getStack().isOf(Items.AIR)) {
+                    for (Integer zb : zhuangbei) {
+                        if (zb == -1) {
+                            for (Integer zby : zhuangbei) {
+                                if (zby == air) break a;
+                            }
+//                            System.out.println("头盔：" + zhuangbei.get(0));
+//                            System.out.println("胸甲：" + zhuangbei.get(1));
+//                            System.out.println("裤子：" + zhuangbei.get(2));
+//                            System.out.println("鞋子：" + zhuangbei.get(3));
+//                            System.out.println("空位：" + air);
+                            autoSwitch(q, air);
+                            zhuangbei.set(q - 5, air);
+                            break a;
+                        }
+                    }
                 }
             }
-        }
-        if (air == -1) return;
-        autoSwitch(q, air);
-        switch (q) {
-            case 5 -> toukui = air;
-            case 6 -> xiongjia = air;
-            case 7 -> kuzi = air;
-            case 8 -> xiezi = air;
         }
     }
 
@@ -125,6 +117,10 @@ public class Auto {
     public Auto(MinecraftClient client) {
         this.mc = client;
         this.player = client.player;
+        zhuangbei = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            zhuangbei.add(i, -1);
+        }
         auto = this;
     }
 
